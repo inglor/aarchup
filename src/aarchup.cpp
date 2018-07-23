dad39334fe0e04dcc56ee32ae1f0f1fff49498bb
Leonidas Spyropoulos <artafinde@gmail.com>
2018-07-23 08:19:39 +0100

    Support old args - only aur works for now

--- src/aarchup.cpp
+++ src/aarchup.cpp
@@ -16,46 +16,45 @@
 #define VERSION_NUMBER "2.0.0"
 
 /* Prints the help. */
-int print_help(char *name) {
-    printf("Usage: %s [options]\n\n", name);
-    printf("Options:\n");
-    printf("          --command|-c [value]        Set the command which gives out the list of updates.\n");
-    printf("                                      The default is /usr/bin/checkupdates\n");
-    printf("          --icon|-p [value]           Shows the icon, whose path has been given as value, in the notification.\n");
-    printf("                                      By default no icon is shown.\n");
-    printf("          --maxentries|-m [value]     Set the maximum number of packages which shall be displayed in the notification.\n");
-    printf("                                      The default value is 30.\n");
-    printf("          --timeout|-t [value]        Set the timeout after which the notification disappers in seconds.\n");
-    printf("                                      The default value is 3600 seconds, which is 1 hour.\n");
-    printf("          --uid|-i [value]            Set the uid of the process.\n");
-    printf("                                      The default is to keep the uid of the user who started aarchup.\n");
-    printf("                                      !!You should change this if root is executing aarchup!!\n");
-    printf("          --urgency|-u [value]        Set the libnotify urgency-level. Possible values are: low, normal and critical.\n");
-    printf("                                      The default value is normal. With changing this value you can change the color of the notification.\n");
-    printf("          --loop-time|-l [value]      Using this the program will loop indefinitely. Just use this if you won't use cron.\n");
-    printf("                                      The value should be the number of minutes from each update check, if none is informed 60 will be used.\n");
-    printf("                                      For more information on it check man.\n");
-    printf("          --help                      Prints this help.\n");
-    printf("          --version                   Shows the version.\n");
-    printf("          --aur                       Check aur for new packages too. Will need auracle installed.\n");
-    printf("          --debug|-d                  Print debug info.\n");
-    printf("          --ftimeout|-f [value]       Program will manually enforce timeout for closing notification.\n");
-    printf("                                      Do NOT use with --timeout, if --timeout works or without --loop-time [value].\n");
-    printf("                                      The value for this option should be in minutes.\n");
-    printf("          --pkg-no-ignore             If this flag is set will not use the IgnorePkg variable from pacman.conf. Without the flag will ignore those\n");
-    printf("                                      packages\n");
-    printf("\nMore informations can be found in the manpage.\n");
+int PrintHelp(char *name) {
+    std::cout << "Usage: aarchup [options]\n\n"
+                 "Options:\n"
+                 "          --command|-c [value]        Set the command which gives out the list of updates.\n"
+                 "                                      The default is /usr/bin/checkupdates\n"
+                 "          --icon|-p [value]           Shows the icon, whose path has been given as value, in the notification.\n"
+                 "                                      By default no icon is shown.\n"
+                 "          --maxentries|-m [value]     Set the maximum number of packages which shall be displayed in the notification.\n"
+                 "                                      The default value is 30.\n"
+                 "          --timeout|-t [value]        Set the timeout after which the notification disappers in seconds.\n"
+                 "                                      The default value is 3600 seconds, which is 1 hour.\n"
+                 "          --uid|-i [value]            Set the uid of the process.\n"
+                 "                                      The default is to keep the uid of the user who started aarchup.\n"
+                 "                                      !!You should change this if root is executing aarchup!!\n"
+                 "          --urgency|-u [value]        Set the libnotify urgency-level. Possible values are: low, normal and critical.\n"
+                 "                                      The default value is normal. With changing this value you can change the color of the notification.\n"
+                 "          --loop-time|-l [value]      Using this the program will loop indefinitely. Just use this if you won't use cron.\n"
+                 "                                      The value should be the number of minutes from each update check, if none is informed 60 will be used.\n"
+                 "                                      For more information on it check man.\n"
+                 "          --help                      Prints this help.\n"
+                 "          --version                   Shows the version.\n"
+                 "          --aur                       Check aur for new packages too. Will need auracle installed.\n"
+                 "          --debug|-d                  Print debug info.\n"
+                 "          --ftimeout|-f [value]       Program will manually enforce timeout for closing notification.\n"
+                 "                                      Do NOT use with --timeout, if --timeout works or without --loop-time [value].\n"
+                 "                                      The value for this option should be in minutes.\n"
+                 "          --pkg-no-ignore             If this flag is set will not use the IgnorePkg variable from pacman.conf. Without the flag will ignore those\n"
+                 "                                      packages\n"
+                 "\nMore information can be found in the manpage.\n";
     exit(0);
 }
 
 /* Prints the version. */
 int print_version() {
-    printf("aarchup %s\n", VERSION_NUMBER);
-    printf("Copyright 2011 aericson <de.ericson@gmail.com>\n");
-    printf("Copyright 2010 Rorschach <r0rschach@lavabit.com>,\n2011 Andrew Kravchuk <awkravchuk@gmail.com>\n");
-    printf("License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n");
-    printf("This is free software: you are free to change and redistribute it.\n");
-    printf("There is NO WARRANTY, to the extent permitted by law.\n");
+    std::cout << "aarchup " VERSION_NUMBER
+                 "\nCopyright 2018 Leonidas Spyropoulos <artafinde@gmail.com>\n"
+                 "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n"
+                 "This is free software, you are free to change and redistribute it.\n"
+                 "There is NO WARRANTY, to the extent permitted by law.\n";
     exit(0);
 }
 
@@ -63,7 +62,7 @@ int print_version() {
 int main(int argc, char **argv) {
     unsigned int timeout = 3600 * 1000;
     /* Restricts the number of packages which should be included in the desktop notification.*/
-    unsigned int max_number_out = 30;
+    int max_number_out = 30;
     unsigned int loop_time = 3600;
     unsigned int manual_timeout = 0;
     const char *command = "/usr/bin/checkupdates";
@@ -77,36 +76,46 @@ int main(int argc, char **argv) {
     static int ignore_pkg_flag = 1;
 
     /* Parse commandline options */
-    int c;
+    const char *const short_opts = "c:p:m:t:i:u:l:df:";
+    const option long_opts[] = {
+            {"command",       required_argument, nullptr,          'c'},
+            {"icon",          required_argument, nullptr,          'p'},
+            {"maxentries",    required_argument, nullptr,          'm'},
+            {"timeout",       required_argument, nullptr,          't'},
+            {"uid",           required_argument, nullptr,          'i'},
+            {"urgency",       required_argument, nullptr,          'u'},
+            {"loop-time",     required_argument, nullptr,          'l'},
+            {"help",          no_argument,       &help_flag,       1},
+            {"version",       no_argument,       &version_flag,    1},
+            {"aur",           no_argument,       &aur,             1},
+            {"ftimeout",      required_argument, nullptr,          'f'},
+            {"debug",         no_argument,       nullptr,          'd'},
+            {"pkg-no-ignore", no_argument,       &ignore_pkg_flag, 0},
+            {nullptr, 0,                         nullptr,          0},
+    };
+
     while (true) {
-        /* Long opts */
-        static struct option long_options[] = {
-                {"command",       required_argument, nullptr,          'c'},
-                {"icon",          required_argument, nullptr,          'p'},
-                {"maxentries",    required_argument, nullptr,          'm'},
-                {"timeout",       required_argument, nullptr,          't'},
-                {"uid",           required_argument, nullptr,          'i'},
-                {"urgency",       required_argument, nullptr,          'u'},
-                {"loop-time",     required_argument, nullptr,          'l'},
-                {"help",          no_argument,       &help_flag,       1},
-                {"version",       no_argument,       &version_flag,    1},
-                {"aur",           no_argument,       &aur,             1},
-                {"ftimeout",      required_argument, nullptr,          'f'},
-                {"debug",         no_argument,       nullptr,          'd'},
-                {"pkg-no-ignore", no_argument,       &ignore_pkg_flag, 0},
-                {nullptr, 0,                         nullptr,          0},
-        };
         int option_index = 0;
-        c = getopt_long(argc, argv, "c:p:m:t:i:u:l:df:", long_options, &option_index);
-        if (c == -1) {
+        const auto opt = getopt_long(argc, argv, short_opts, long_opts, &option_index);
+        if (-1 == opt) {
             break;
         }
         /* Short opts */
-        switch (c) {
+        switch (opt) {
             case 0:
-                if (long_options[option_index].flag) {
+                if (long_opts[option_index].flag) {
                     break;
                 }
+                if (debug) {
+                    printf("DEBUG(error): erroropt: option %s", long_opts[option_index].name);
+                    if (optarg) {
+                        printf(" with args %s", optarg);
+                    }
+                    printf("\n");
+                }
+                break;
+            case 'v':
+                print_version();
                 break;
             case 'c':
                 command = optarg;
@@ -119,7 +128,7 @@ int main(int argc, char **argv) {
                     printf("--maxentries argument needs to be a number\n");
                     exit(1);
                 }
-                max_number_out = atoi(optarg);
+                max_number_out = std::stoi(optarg);
                 break;
             case 't':
                 if (!isdigit(optarg[0])) {
@@ -176,11 +185,11 @@ int main(int argc, char **argv) {
                     exit(1);
                 }
                 break;
-            case '?':
-                print_help(argv[0]);
-                break;
+            case 'h': // -h or --help
+            case '?': // Unrecognized option
             default:
-                exit(1);
+                PrintHelp(argv[0]);
+                break;
         }
     }
 
@@ -194,8 +203,16 @@ int main(int argc, char **argv) {
     }
     if (!checkUpdateOut.empty() || (!aurHelperOut.empty())) {
         notify_init("Aarchup");
+        std::string finalOut;
+        if (aurHelperOut.empty()) {
+            finalOut = checkUpdateOut;
+        } else if (checkUpdateOut.empty()) {
+            finalOut = "AUR updates:\n"+aurHelperOut;
+        } else {
+            finalOut = checkUpdateOut+"\nAUR updates:\n"+aurHelperOut;
+        }
         NotifyNotification *n = notify_notification_new("New updates for Arch Linux available!",
-                                                        checkUpdateOut.c_str(), nullptr);
+                                                        finalOut.c_str(), nullptr);
         notify_notification_set_timeout(n, 5000);
         notify_notification_set_urgency(n, urgency);
         if (!notify_notification_show(n, nullptr)) {
